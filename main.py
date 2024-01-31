f60801b9c55c139ceccbb6e4b2b1489010605469
hjeane <hyojinyang9898@gmail.com>
2024-01-31 14:12:18 +0900

    jinja2기본

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!!"
+    return render_template('index.html',)
+
+@app.route('/hello')
+def hello():
+    return "안녕 반갑다!!"
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
