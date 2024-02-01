de892004f2d657e94f7d6528d76f9a5eb4ece8f2
hjeane <hyojinyang9898@gmail.com>
2024-02-01 14:22:19 +0900

    getpost

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html',)
+    return render_template('index.html',name="양효뿅")
 
-@app.route('/hello')
-def hello():
-    return "안녕 반갑다!!"
+@app.route('/hello/<name>')
+def hello(name):
+    #print(action, sound)
+    action = request.args.get('action')
+    sound = request.args.get('sound')
+    return render_template('hello.html',
+                           data={'name':name,
+                                 'action':action,
+                                   'sound':sound})
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        print(username, password)
+        return "Success"
+    elif request.method == 'GET':
+        return render_template('signin.html')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        print(username, password)
+        return email
+    
+    elif request.method == 'GET':
+        return render_template('register.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    app.run(debug=True)    
